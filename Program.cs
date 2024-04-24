e3411ccd21221bf3c89e7acecaaf6f9ceeb05a4c
diegodomingostbl <diegodomingostbl@gmail.com>
2024-04-24 00:39:37 -0300

    Adicionar smartphones Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+nokia.InstalarAplicativo("Waze");
+
+Console.WriteLine("\nSmartphone iPhone");
+Smartphone iphone = new Iphone("654321", "Modelo 2", "222222", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+iphone.InstalarAplicativo("YouTube");
